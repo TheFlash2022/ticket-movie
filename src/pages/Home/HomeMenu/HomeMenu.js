63fe74c0645ee26a7b3043b9e6dfa776db8e9fa2
LeAnhSinh <sinhla213@gmail.com>
2022-07-07 18:12:01 +0700

    42

--- src/pages/Home/HomeMenu/HomeMenu.js
+++ src/pages/Home/HomeMenu/HomeMenu.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { Fragment, useEffect } from "react";
 import { Radio, Space, Tabs } from "antd";
 import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import moment from "moment";
 
 const { TabPane } = Tabs;
 
@@ -8,23 +10,98 @@ export default function HomeMenu(props) {
   const [state, setState] = useState({
     tabPosition: "left",
   });
+
   const changeTabPosition = (e) => {
     setState({ tabPosition: e.target.value });
   };
+
+  useEffect(() => {
+    console.log(props, "props123");
+  });
+
+  const renderHeThongRap = () => {
+    return props.heThongRapChieu?.map((heThongRap, index) => {
+      return (
+        <TabPane
+          tab={
+            <img src={heThongRap.logo} className="rounded-full" width="50" />
+          }
+          key={index}
+        >
+          <Tabs tabPosition={tabPosition}>
+            {heThongRap.lstCumRap?.map((cumRap, index) => {
+              return (
+                <TabPane
+                  tab={
+                    <div style={{ width: "300px", display: "flex" }}>
+                      <img src={heThongRap.logo} width="50" />
+                      <br />
+                      <div className="text-left ml-2">
+                        {cumRap.tenCumRap}
+                        <p className="text-red-200">Chi tiết</p>
+                      </div>
+                    </div>
+                  }
+                  key={index}
+                >
+                  {cumRap.danhSachPhim.slice(0, 4).map((phim, index) => {
+                    return (
+                      <Fragment key={index}>
+                        <div className="my-5">
+                          <div style={{ display: "flex" }}>
+                            <img
+                              style={{ height: 75, width: 75 }}
+                              src={phim.hinhAnh}
+                              alt={phim.tenPhim}
+                              onError={(e) => {
+                                e.target.onError = null;
+                                e.target.src = "https://picsum.photo/75/75";
+                              }}
+                            />
+                            <div className="ml-2">
+                              <h1 className="text-2xl text-green-700">
+                                {phim.tenPhim}
+                              </h1>
+                              <p>{cumRap.diaChi}</p>
+
+                              <div className="grid grid-cols-6 gap-6">
+                                {phim.lstLichChieuTheoPhim
+                                  ?.slice(0, 12)
+                                  .map((lichChieu, index) => {
+                                    return (
+                                      <NavLink
+                                        className="text-l text-green-400"
+                                        to="/"
+                                        key={index}
+                                      >
+                                        {moment(
+                                          lichChieu.ngayChieuGioChieu
+                                        ).format("hh:mm A")}
+                                      </NavLink>
+                                    );
+                                  })}
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <hr />
+                      </Fragment>
+                    );
+                  })}
+                </TabPane>
+              );
+            })}
+          </Tabs>
+        </TabPane>
+      );
+    });
+  };
+
   const { tabPosition } = state;
+
   return (
     <>
-      <Tabs tabPosition={tabPosition}>
-        <TabPane tab="Tab 1" key="1">
-          Content of Tab 1
-        </TabPane>
-        <TabPane tab="Tab 2" key="2">
-          Content of Tab 2
-        </TabPane>
-        <TabPane tab="Tab 3" key="3">
-          Content of Tab 3
-        </TabPane>
-      </Tabs>
+      <Tabs tabPosition={tabPosition}>{renderHeThongRap()}</Tabs>
     </>
   );
 }
